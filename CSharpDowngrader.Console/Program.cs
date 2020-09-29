e13499a8913201559ae761300f85f184f0d7617e
Sören Kuklau <kuklau@trenz.de>
2020-09-30 00:24:49 +0200

    Add project files.

---
+++ CSharpDowngrader.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Redacted.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            using (var sr = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("CSharpDowngrader.Console.CSharp8Sample.cs")))
+                text = sr.ReadToEnd();
+
+            var tree = CSharpSyntaxTree.ParseText(text);
+            SyntaxNode rootNode = tree.GetRoot();
+            foreach (var nullableTypeSyntaxNode in rootNode.DescendantNodes().OfType<Microsoft.CodeAnalysis.CSharp.Syntax.NullableTypeSyntax>())
+            {
+                rootNode = rootNode.ReplaceNode(nullableTypeSyntaxNode, nullableTypeSyntaxNode.ElementType);
+            }
+
+            var result = rootNode.ToFullString();
+        }
+    }
+}
